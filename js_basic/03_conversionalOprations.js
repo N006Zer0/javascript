4fc738624cb98a42ab3b7a6189640ae7ed6a2562
N006Zer0 <85049833+N006Zer0@users.noreply.github.com>
2023-09-25 08:12:45 +0000

    NaN not a number

--- js_basic/03_conversionalOprations.js
+++ js_basic/03_conversionalOprations.js
@@ -1,6 +1,24 @@
 let yourAge = 31; // its number because its without ""
 
-console.log(yourAge);
-console.log(typeof yourAge);
+//console.log(yourAge);
+//console.log(typeof yourAge);
 
-// supposed that we write number with "" 
+// supposed that we write number with ""  its become string
+
+// yourAge ="31a" //  number in string format
+// console.log(typeof yourAge);
+
+// now we always want to treat yourAge as number we do
+
+let ageInNumber = Number(yourAge); // Number , String, Boolen start with capital for conversion
+
+//console.log(yourAge);
+//console.log(typeof yourAge);
+
+// console.log(typeof ageInNumber);
+// console.log(ageInNumber)
+
+// NaN not a number
+
+
+
