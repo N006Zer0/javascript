fdb5d416098bc399f98ddefa8ce93651081187b3
N006Zer0 <85049833+N006Zer0@users.noreply.github.com>
2023-09-26 13:18:49 +0000

    Bigint

--- js_basic/02_dataTypes.js
+++ js_basic/02_dataTypes.js
@@ -2,13 +2,39 @@
 In this page we disscuss about datatype in Javascrit
 */
 
+/*
+
+In javaScript Having 2 datatypes
+1. Primitive DataTypes.
+2. Non Premitive or Refrence DataTypes.
+
+----------------------------------------
+Primitive Datatypes are 7
+----------------------------------------
+Number - 12345 etc. are called numbers
+String - "in between" are string
+Boolean - True or False or 1 and 0
+Null - its null not zero its Empty
+Undifined - veriable gernarted but value not present
+Symbol - special types object datatpre
+Bigint - its also a number but larger numbers end of number put n - 123n
+------------------------------------------
+
+
+*/
+
+
 let yourName = "Ashish Chaturvedi" // string = use into ""
 let yourAge = 31 //number = only number
 let creditCard = true //boolen = true/false
 let contactNumber = null //null = object
 let accountNumber;//undefined = undefined
 //symbol  = spaical used in  react or another programs
+let a = 1n
 
 
 
-console.table([yourName,yourAge,creditCard,contactNumber,accountNumber])
+
+console.table([yourName,yourAge,creditCard,contactNumber,accountNumber,a])
+console.table([typeof yourName, typeof yourAge, typeof creditCard, typeof contactNumber, typeof accountNumber, typeof a])
+
